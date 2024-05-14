306670143fbc745b27fd7ec038b6d60a45f6e3ae
Omri Cohen <omricohen19@gmail.com>
2024-05-14 19:46:19 +0300

    first push

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const PORT = process.env.PORT ?? 8000
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const cors = require('cors')
+const app = express()
+const pool = require('./db')
+
+app.use(cors())
+app.use(express.json)
+
+// get all to-dos
+app.get('/todos/:userEmail', async (req, res) => {
+    const { userEmail } = req.params
+    try {
+      const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+      res.json(todos.rows) 
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+// create a new to-do
+app.post('/todos', async (req, res) => {
+  const {user_email, title, progress, date} = req.body
+  console.log(user_email, title, progress, date)
+  const id = uuidv4()
+  try {
+    const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
+      [id, user_email, title, progress, date])
+    res.json(newToDo)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
